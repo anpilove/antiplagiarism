d83a7e8e9a06ff4506fba1be6ffa220fdbc99565
kirillanpilov <anpilov.kirill2002@gmail.com>
2023-01-08 20:20:07 +0300

    Add LevenshteinDistance

--- compare.py
+++ compare.py
@@ -1,8 +1,40 @@
 import argparse
-import numpy
 import ast
 import tokenize
 import re
+
+import numpy as np
+
+
+def LevenshteinDistance(code1, code2):
+    m, n = len(code1), len(code2)
+    generator_array = [[0] * (n + 1) for i in range(m + 1)]
+    array = np.array(generator_array)
+
+    for i in range(m + 1):
+        array[i][0] = i
+    for j in range(n + 1):
+        array[0][j] = j
+
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if code1[i - 1] == code2[j - 1]:
+                array[i][j] = array[i - 1][j - 1]
+            else:
+                array[i][j] = 1 + min(array[i - 1][j], array[i][j - 1], array[i - 1][j - 1])
+
+    return array[-1][-1]
+
+class DictVisitor(ast.NodeVisitor):
+
+    def visit_Dict(self,node):
+        print('Node type: Dict\nFields: ', node._fields)
+        ast.NodeVisitor.generic_visit(self, node)
+
+
+    def visit_Constant(self,node):
+        print('Node type: Constant\nFields: ', node._fields)
+        ast.NodeVisitor.generic_visit(self, node)
 
 
 class Visitor(ast.NodeVisitor):
@@ -25,12 +57,13 @@ def input_file_proc(name: str) -> list:
     return list_files
 
 
-def file_proc(file_name: str):
-    print(file_name)
-    with open(file_name) as file:
+def file_proc(file_path: str):
+    print(file_path)
+    with open(file_path) as file:
         code = file.read()
 
     node = ast.parse(code)
+    print(ast.dump(node, indent=4, ))
     list1 = Visitor().visit(node)
     # print(len(list1))
     # print(node)
@@ -56,9 +89,11 @@ def compare(list_pair: list, file_name: str):
         list2 = file_proc(file_path_text2)
         for i in range(len(list2)):
             if list1[i] != list2[i]:
-                print(list1[i])
-                print(list2[i])
-                print()
+                print(sorted(list1[i].split(',')))
+                print(sorted(list2[i].split(',')))
+                word1 = " ".join(sorted(list1[i].split(',')))
+                word2 = " ".join(sorted(list2[i].split(',')))
+                print(LevenshteinDistance(word1,word2) , len(word1))
         break
 
 
@@ -71,8 +106,12 @@ def compare(list_pair: list, file_name: str):
 #
 # input_file = args.input_file
 # output_file = args.output_file
-input_file = "input.txt"
-output_file = "output.txt"
-list_files = input_file_proc(input_file)
 
-compare(list_files, output_file)
+
+# input_file = "input.txt"
+# output_file = "output.txt"
+# list_files = input_file_proc(input_file)
+#
+# compare(list_files, output_file)
+
+
